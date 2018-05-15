2e83f1b2902a2f542a64a9abcb24040c8b5c9ea5
Sclarke5 <sclarke5555@gmail.com>
2018-05-15 19:43:28 -0400

    finished, fixed event listener

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
+var button = document.querySelector('button');
+var background = document.querySelector('.light');
+var text = document.querySelector('h1');
 
+function switchLight(){
+  if (button.className == 'switch on'){
+    background.className = 'dark';
+    text.innerText = 'Hey, who turned off the lights?';
+    button.className = 'switch off';
+  }
+  else if (button.className == 'switch off'){
+    background.className = 'light';
+    text.innerText = "It's so bright in here!";
+    button.className = 'switch on';
+  }
+}
 
+button.addEventListener('click', switchLight);
